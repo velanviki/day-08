8891ad3181a3939c55f951096da05647f6b66c39
velanviki <92292839+velanviki@users.noreply.github.com>
2021-12-29 19:32:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+class person{
+    constructor(name,age,gender,education){
+        this.Name=name;
+        this.Age=age;
+        this.Gender=gender;
+        this.Education=education
+    }
+}
+
+let ref1 = new person("Velan",20,"Male","B-Tech");
+console.log(ref1);
